363130f0b1597b3c6b62313277c762f1641178e0
José Miguel Ramos <miguelyinyang@gmail.com>
2026-07-15 13:59:28 +0200

    Load Flask secret key from environment

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import os
 
 app = Flask(__name__)
 
-app.secret_key = 'joséramos'
+app.secret_key = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = (
